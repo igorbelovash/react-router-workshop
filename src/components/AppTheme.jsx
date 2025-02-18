0230fa392d13be6bfcda48133f1b760619c162d1
igorbelovash <108266576+igorbelovash@users.noreply.github.com>
2025-02-18 11:55:30 +0200

    Add Login page

---
+++ src/components/AppTheme.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+export default function AppTheme(props) {
+  const { children, disableCustomTheme, themeComponents } = props;
+  const theme = React.useMemo(() => {
+    return disableCustomTheme
+      ? {}
+      : createTheme({
+          // For more details about CSS variables configuration, see https://mui.com/material-ui/customization/css-theme-variables/configuration/
+          cssVariables: {
+            colorSchemeSelector: 'data-mui-color-scheme',
+            cssVarPrefix: 'template',
+          },
+        });
+  }, [disableCustomTheme, themeComponents]);
+  if (disableCustomTheme) {
+    return <React.Fragment>{children}</React.Fragment>;
+  }
+  return (
+    <ThemeProvider theme={theme} disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  );
+}
